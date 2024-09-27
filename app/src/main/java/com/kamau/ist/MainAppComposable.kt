618ac88d51df76c8f0fd903c9082b98528a786c6
kamau <githuakamau99@gmail.com>
2024-09-27 05:16:33 +0300

    Set up the login signup and home navigation composables functions

--- app/src/main/java/com/kamau/ist/MainAppComposable.kt
+++ app/src/main/java/com/kamau/ist/MainAppComposable.kt
@@ -1,8 +1,34 @@
 package com.kamau.ist
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.google.firebase.auth.FirebaseAuth
+import com.kamau.ist.feature.auth.signin.SignInScreen
+import com.kamau.ist.feature.auth.signup.SignUpScreen
+import com.kamau.ist.feature.home.HomeScreen
 
 @Composable
-fun MainApp(){
+fun MainApp() {
+    Surface(modifier = Modifier.fillMaxSize()) {
+        val navController = rememberNavController()
+        val currentUser = FirebaseAuth.getInstance().currentUser
+        val start = if (currentUser != null) "home" else "login"
+        NavHost(navController = navController, startDestination = start) {
 
+            composable("login") {
+                SignInScreen(navController)
+            }
+            composable("signup") {
+                SignUpScreen(navController)
+            }
+            composable("home") {
+                HomeScreen(navController)
+            }
+        }
+    }
 }
