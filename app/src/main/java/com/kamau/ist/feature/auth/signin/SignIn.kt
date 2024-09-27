445aa725c6418d61ad5eb8cbdb69e21ec4d89b04
kamau <githuakamau99@gmail.com>
2024-09-27 04:16:48 +0300

    Added a feature package and inside it I added an auth package
    Added an ist class where I the HiltAndroidApp is called

---
+++ app/src/main/java/com/kamau/ist/feature/auth/signin/SignIn.kt
@@ -0,0 +1,2 @@
+package com.kamau.ist.feature.auth.signin
+
---
+++ app/src/main/java/com/kamau/ist/ist.kt
@@ -0,0 +1,8 @@
+package com.kamau.ist
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ist: Application()  {
+}
