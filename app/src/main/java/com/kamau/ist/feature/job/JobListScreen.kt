2f86c36bc2d83c2e589515d6cb8159d31fd950dd
kamau <githuakamau99@gmail.com>
2024-09-28 10:06:48 +0300

    Added JobListScreen kotlin file

--- app/src/main/java/com/kamau/ist/feature/job/JobListScreen.kt
+++ app/src/main/java/com/kamau/ist/feature/job/JobListScreen.kt
@@ -1,2 +1,56 @@
 package com.kamau.ist.feature.job
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+import com.kamau.ist.model.Job
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun JobListScreen(navController: NavController, viewModel: JobViewModel = hiltViewModel()) {
+    Scaffold(
+        topBar = {
+            TopAppBar(title = { Text("Job Listings") })
+        },
+        content = {
+            LazyColumn(modifier = Modifier.fillMaxSize()
+                .padding(it)) {
+                items(viewModel.jobList) { job ->
+                    JobItem(job = job, onClick = { navController.navigate("job_detail/${job.id}") })
+                }
+            }
+        }
+    )
+}
+
+@Composable
+fun JobItem(job: Job, onClick: () -> Unit) {
+    Card(
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxWidth()
+            .clickable { onClick() },
+//        elevation = 4.dp
+    ) {
+        Column(modifier = Modifier.padding(16.dp)) {
+            Text(text = job.title)
+            Text(text = "Company: ${job.company}")
+            Text(text = "Deadline: ${job.deadline}")
+        }
+    }
+}
