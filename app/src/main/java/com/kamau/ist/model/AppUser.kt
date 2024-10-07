8c1290afa95045f82b468488d898190367790574
kamau <githuakamau99@gmail.com>
2024-10-07 09:00:50 +0300

    Created profilePictureUrl function in the AppUser data class

--- app/src/main/java/com/kamau/ist/model/AppUser.kt
+++ app/src/main/java/com/kamau/ist/model/AppUser.kt
@@ -3,6 +3,7 @@ package com.kamau.ist.model
 data class AppUser(
     val uid: String = "",
     val email: String = "",
+    val profilePictureUrl: String = "",
 // Possible values: "Admin" or "User"
     val role: String = ""
 )
